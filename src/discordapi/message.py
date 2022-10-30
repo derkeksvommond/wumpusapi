87a96daa8e724d731843e39bae7eda43c8b6e82d
Mondkeks <81697210+derkeksvommond@users.noreply.github.com>
2022-10-31 00:03:11 +0100

    Create message.py

---
+++ src/discordapi/message.py
@@ -0,0 +1,13 @@
+class Field:
+    def __init__(self, name, value):
+        pass
+
+
+class Embed:
+    def __init__(self, title, description, fields=None):
+        pass
+
+
+class Message:
+    def __init__(self, content, tts=False, embeds=None, components=None):
+        pass
